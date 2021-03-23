07ec7aa2616f91a4c80586171da545909da9ebe1
Ricardo Dalinger <rdalinger@siu.edu.ar>
2021-03-23 18:56:25 -0300

    Fix para que reconozca date.timezone

--- setup_php_ini.sh
+++ setup_php_ini.sh
@@ -9,6 +9,6 @@ sed -i \
  -e "s|;*post_max_size =.*|post_max_size = \$\{PHP_MAX_POST\}|i" \
  -e "s|;*display_errors =.*|display_errors = \$\{PHP_DISPLAY_ERRORS\}|i" \
  -e "s|;*log_errors =.*|log_errors = \$\{PHP_LOGS_ERRORS\}|i" \
- -e "s|date.timezone =.*|date.timezone = \$\{TZ\}|i" \
- -e "s|*expose_php =.*|expose_php = Off|i" \
+ -e "s|;*date.timezone  =.*|date.timezone = \$\{TZ\}|i" \
+ -e "s|expose_php =.*|expose_php = Off|i" \
     /etc/php7/php.ini
