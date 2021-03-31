7a3d4ceb3fc2d3c621b84a8fd2c672b58b105cb1
Ricardo Dalinger <rdalinger@siu.edu.ar>
2021-03-31 12:23:54 -0300

    Modifica el nombre de la env-var y ajusta script + defaults

--- setup_php_ini.sh
+++ setup_php_ini.sh
@@ -3,7 +3,7 @@
 # Set environments variables in config file
 sed -i \
  -e "s|;*memory_limit =.*|memory_limit = \$\{PHP_MEMORY_LIMIT\}|i" \
- -e "s|;*upload_max_filesize =.*|upload_max_filesize = \$\{MAX_UPLOAD\}|i" \
+ -e "s|;*upload_max_filesize =.*|upload_max_filesize = \$\{PHP_MAX_UPLOAD_SIZE\}|i" \
  -e "s|;*max_file_uploads =.*|max_file_uploads = \$\{PHP_MAX_FILE_UPLOAD\}|i" \
  -e "s|;*max_input_vars =.*|max_input_vars = \$\{PHP_MAX_INPUT_VARS\}|i" \
  -e "s|;*post_max_size =.*|post_max_size = \$\{PHP_MAX_POST\}|i" \
