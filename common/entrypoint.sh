77c0640f493361cb27fab2153c3cc862d83fdf3b
Christian Rodriguez <chrodriguez@gmail.com>
2021-05-21 10:59:46 -0300

    Aplicamos prácticas de seguridad en contenedor
    En este commit permitimos correr con cualquier usuario no privilegiado
    al contenedor. La mejora aplica a la versión rootless y considera que el
    usuario que usa para correr pueda tener un UID random. De hecho propone
    poder correr la imagen de la siguiente forma:
    
    docker run -u $((RANDOM +1)) --rm -it siutoba/php:xxxx-rootless

--- common/entrypoint.sh
+++ common/entrypoint.sh
@@ -2,6 +2,12 @@
 
 set -eo pipefail
 
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "siu-toba:x:$(id -u):0:SIU toba:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 for i in /siu-entrypoint.d/* ; do
   source $i
 done
